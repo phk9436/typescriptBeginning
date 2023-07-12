ed7651a9a73f781f9924d112fe0cde2b43c1d088
han_kyeul <phk9436phk@gmail.com>
2023-07-13 00:09:46 +0900

    1. 기본 타입 - enum에 정보 추가

--- basic.ts
+++ basic.ts
@@ -67,7 +67,26 @@ const Os2Comfiled = {
 let myOs:Os;
 myOs = Os.Android;
 
+// Os.Android = 11; //error; (enum은 값 재할당 불가능)
+// Os.Watch = 12; //error; (새로운 속성 추가 불가능)
 
+// enum Os3 {
+//     Window = true
+// } //error; (문자나 숫자 외의 값 할당 불가능)
+
+//Object와 enum의 차이
+//1. enum은 object와 달리 새로운 속성을 추가하거나 속성에 값을 재할당할 수 없다.
+//2. object의 속성값은 모든 타입이 올 수 있으나, enum의 속성값은 문자 또는 숫자만 허용된다.
+
+//enum은 열거형 타입으로, 해당 용도로 선언할 때 object보다 명확한 의미 전달이 가능.
+
+const enum Os4 {
+    Window,
+    Ios,
+    Android
+}
+console.log(Os4.Android)
+//const enum을 쓰면 더 간결하게 트랜스파일 된다.
 
 // 4. null, undefined
 let typeNull:null = null;
